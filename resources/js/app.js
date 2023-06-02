983ee3be4a7932bcf362ee55c07856ee2124719d
Leo-the-Limitless <maypeace.mm@gmail.com>
2023-06-02 09:56:17 +0630

    First commit

---
+++ resources/js/app.js
@@ -0,0 +1,36 @@
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+// Import components
+import App from "./components/App.vue";
+import ProductList from "./components/ProductList.vue";
+import ProductForm from "./components/ProductForm.vue";
+import Product from "./components/Product.vue";
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: "/", component: ProductList },
+        { path: "/products/create", component: ProductForm },
+        { path: "/products/:id", component: Product },
+        { path: "/products/:id/edit", component: ProductForm },
+    ],
+});
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("/serviceworker.js")
+            .then((registration) => {
+                console.log(
+                    "ServiceWorker registered with scope:",
+                    registration.scope
+                );
+            })
+            .catch((error) => {
+                console.log("ServiceWorker registration failed:", error);
+            });
+    });
+}
